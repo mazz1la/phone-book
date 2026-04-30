741dbb3446698795f7647dadaf6933b939a2988a
Владислав Хен <gr634_hevro@lab-203-7.tomtit-tomsk.ru>
2026-04-30 10:29:23 +0700

    Delete contact function

--- contacts.py
+++ contacts.py
@@ -67,7 +67,7 @@ def delete_contact():
 
 def main():
     print("=" * 40)
-    print("      ТЕЛЕФОННАЯ КНИГА")
+    print("      ТЕЛЕФОННАЯ КНИГА - DELETE")
     print("=" * 40)
     while True:
         print("\nВыберите действие:")
